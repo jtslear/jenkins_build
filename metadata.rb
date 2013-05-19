d610c1921ddeac233f224257705e6c1b1e8842c5
John T Skarbek <jtslear@gmail.com>
2013-05-18 22:22:21 -0400

    Add database depdendency.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jtslear@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures jenkins_build'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends "apt"
 depends "postgresql"
@@ -12,3 +12,4 @@ depends "jenkins"
 depends "nodejs"
 depends "phantomjs"
 depends "git"
+depends "database"
