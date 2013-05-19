21c1cac30b47713a77b90a8e3a0cabf123165442
Jason Draper <jason@drapergeek.com>
2013-05-18 23:40:59 -0400

    Cleanup formatting or attributes.rb.
    * Move array items onto their own lines.
    * Remove unnecessary comments.
    * Reword comments to be more explanatory.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,40 +1,66 @@
-# configure a specific version of postgres
 node.set['postgresql']['version'] = "9.2"
 
-# allow passwordless authentication to postgres
+# Allow Postgres Password-less Authentication
 node.set['postgresql']['pg_hba'] = [
   {
-  :comment => "# created by chef \'jenkins_build\' for IPv4",
-  :type => 'host', 
-  :db => 'all', 
-  :user => 'all', 
-  :addr => '127.0.0.1/32',
-  :method => 'trust'
-},
+    :comment => "# created by chef \'jenkins_build\' for IPv4",
+    :type => 'host',
+    :db => 'all',
+    :user => 'all',
+    :addr => '127.0.0.1/32',
+    :method => 'trust'
+  },
   {
-  :comment => "# created by chef \'jenkins_build\' for IPv6",
-  :type => 'host', 
-  :db => 'all', 
-  :user => 'all', 
-  :addr => '::1/128',
-  :method => 'trust'
-},
+    :comment => "# created by chef \'jenkins_build\' for IPv6",
+    :type => 'host',
+    :db => 'all',
+    :user => 'all',
+    :addr => '::1/128',
+    :method => 'trust'
+  },
 ]
 
-# the default recipe for ruby_build includes a package, libyaml, that is not available on centos, overwriting it
-node.override['ruby_build']['install_pkgs_cruby'] =
-  %w{ gcc-c++ patch readline readline-devel zlib zlib-devel
-      libffi-devel openssl-devel
-      make bzip2 autoconf automake libtool bison
-      libxml2 libxml2-devel libxslt libxslt-devel
-      git subversion autoconf }
+node.override['ruby_build']['install_pkgs_cruby'] = [
+  "gcc-c++",
+  "patch",
+  "readline",
+  "readline",
+  "devel",
+  "zlib",
+  "zlib",
+  "devel",
+  "libffi",
+  "devel",
+  "openssl",
+  "devel",
+  "make",
+  "bzip2",
+  "autoconf",
+  "automake",
+  "libtool",
+  "bison",
+  "libxml2",
+  "libxml2",
+  "devel",
+  "libxslt",
+  "libxslt-devel",
+  "git",
+  "subversion",
+  "autoconf"
+]
 
-# adding some plugins to jenkins
-# for some odd reason the rbenv and campfire plugins do not install
-node.default[:jenkins][:server][:plugins] = 
-  %w{ git-client git-server git github ruby-runtime rbenv campfire }
+node.default[:jenkins][:server][:plugins] = [
+  'git-client',
+  'git-server',
+  'git',
+  'github',
+  'ruby-runtime',
+  'rbenv',
+  'campfire',
+]
 
-# The below are requirements for some gems
+# Gem Requirements
 node.default['jenkins_build']['packages'] = %w{ libxml2-dev libxslt-dev }
 
+# Jenkins Postgres Password
 node.default['jenkins_build']['jenkins_user_postgresql_password'] = 'superduperpassword'
